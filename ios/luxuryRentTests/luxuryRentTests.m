98babbb42a4b029518ffeab2e4ac079816e8456e
Gabriel Fernandes Lima <gabriel.fernandes.06@outlook.com>
2023-07-14 20:30:49 -0300

    rename project

--- ios/luxuryRentTests/luxuryRentTests.m
+++ ios/luxuryRentTests/luxuryRentTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface orgsHooksTests : XCTestCase
+@interface luxuryRentTests : XCTestCase
 
 @end
 
-@implementation orgsHooksTests
+@implementation luxuryRentTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
